c0a0d8c11c91006e1645e45660b257a3b6741851
jeffmcrockett <jeffmcrockett@gmail.com>
2020-10-08 15:56:20 -0500

    adding typewriter animation still missing text3 and text4

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var checkScrollBars = function(){
+    var b = $('body');
+    var normalw = 0;
+    var scrollw = 0;
+    if(b.prop('scrollHeight')>b.height()){
+        normalw = window.innerWidth;
+        scrollw = normalw - b.width();
+        $('#container').css({marginRight:'-'+scrollw+'px'});
+    }
+}
+
+$(function(){
+    checkScrollBars();
+    $('#showbar').click(function(){
+        $('body').css('overflow-y', 'auto');
+        checkScrollBars();
+    })
+    $('#hidebar').click(function(){
+        $('body').css('overflow-y', 'hidden');
+        checkScrollBars();
+    })
+});
